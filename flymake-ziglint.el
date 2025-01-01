62a79c45f59b62fc441a1b5563734d83c2e0c5b0
JenChieh <jcs090218@gmail.com>
2025-01-01 02:00:23 -0800

    Update copyright year

--- flymake-ziglint.el
+++ flymake-ziglint.el
@@ -1,6 +1,6 @@
 ;;; flymake-ziglint.el --- Flymake for ziglint  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024  Shen, Jen-Chieh
+;; Copyright (C) 2024-2025  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
